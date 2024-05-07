74d87be1dfb2ed043648abf4deb651e99585afb1
Gaurav <program.drgk@gmail.com>
2024-05-07 12:59:12 -0700

    Code clean up DONE

--- routes/route-jobs.ts
+++ routes/route-jobs.ts
@@ -1,31 +1,19 @@
 import { Router } from "express";
-import postJobDto from "../dtos/post-job";
 import * as jobsController from "../controllers/jobsController";
 
 const router = Router();
 
-// router.get("/", (req, res) => res.send("Express with TS server working")); to chk router works
-
-// router.get("/dashboard", (req, res) => res.send("All jobs ")); simple test way
+// route to get all job postings
 router.get("/dashboard", jobsController.getAllJobs);
 
-// router.get("/dashboard/:id", (req, res) => res.send("Individual job details ")); simple test way
+// route to get individual job posting by id
 router.get("/dashboard/:id", jobsController.getSingleJob);
-router.put("/dashboard/:id", jobsController.editJob);
 
-// router.post("/dashboard", (req, res) => {
-//   const {
-//     company_name,
-//     job_position,
-//     date,
-//     status,
-//     role,
-//     duties,
-//     requirements,
-//   } = req.body as postJobDto;
-//   res.status(201).json({ message: `New Job posted to dashboard` });
-// });
+// route to post new job posting
 router.post("/dashboard", jobsController.postJob);
+
+// route to edit existing job posting
+router.put("/dashboard/:id", jobsController.editJob);
 
 // module.exports = router;
 export default router;
